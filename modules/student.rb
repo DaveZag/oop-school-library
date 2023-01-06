cf400246383ee05e2522d4ef8c2492958f31a59b
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-01-07 01:19:52 +0200

    Create basic UI

--- modules/student.rb
+++ modules/student.rb
@@ -3,9 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
   end
 
   def classroom=(classroom)
