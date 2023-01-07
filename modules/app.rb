7c113203e1e5a40507a24b89f5c76a16ce8e7026
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-01-07 14:50:52 +0200

    Fix linters errors and test cases

--- modules/app.rb
+++ modules/app.rb
@@ -6,9 +6,8 @@ require_relative 'book'
 class App
   def initialize
     @books = []
-    @rentals = []
     @people = []
-    
+
     @welcome_message = "
     Please choose an option entering a number:
     1 - List all books
@@ -23,17 +22,17 @@ class App
 
   def list_books
     if @books.empty?
-      puts "No book found. Please insert one." 
+      puts 'No book found. Please insert one.'
     else
       @books.each_with_index do |book, index|
         puts "#{index}) Title: \"#{book.title}\", Author: #{book.author} "
-      end 
+      end
     end
   end
 
   def list_people
     if @people.empty?
-      puts "No one was found. Please create a person then try again."
+      puts 'No one was found. Please create a person then try again.'
     else
       @people.each_with_index do |person, index|
         puts "#{index})- [#{person.class}] Name: #{person.name}, ID:#{person.id}, Age:#{person.age}"
@@ -42,27 +41,31 @@ class App
   end
 
   def add_student
-    print "Age: "
+    print 'Age: '
     age = gets.chomp.to_i
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
-    print "Has parent permission? [Y/N]: "
-    has_permission = gets.chomp.uppercase
+    print 'Has parent permission? [Y/N]: '
+    has_permission = gets.chomp.upcase
 
     case has_permission
-    when 'Y' 
+    when 'Y'
       parent_permission = true
     when 'N'
       parent_permission = false
+    else
+      'Unknown input'
     end
+
+    puts "#{age}, #{name}, #{parent_permission}"
 
     @people.push(Student.new(age, name, parent_permission))
   end
 
   def add_teacher
-    print "Age: "
+    print 'Age: '
     age = gets.chomp.to_i
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
     print 'Specialization: '
     specialization = gets.chomp
@@ -70,9 +73,9 @@ class App
     @people.push(Teacher.new(specialization, age, name))
   end
 
-  #create a person
+  # create a person
   def create_person
-    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     person_type = gets.chomp.to_i
 
     case person_type
@@ -85,15 +88,44 @@ class App
     puts 'Person created successfully'
   end
 
+  # create book
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+
+    @books.push(Book.new(title, author))
+  end
+
+  # create rental
+  def create_rental
+    puts 'Select a book from the following list by number:'
+    list_books
+    book_id = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number(not ID)'
+    list_people
+    person_id = gets.chomp.to_i
+
+    print 'Date(YY/MM/DD): '
+    rental_date = gets.chomp
+
+    Rental.new(person_id, book_id, rental_date)
+  end
+
+  # list people with their rentals
   def list_person_rentals
     print "Person's ID:"
     id = gets.chomp.to_i
-    puts "Rentals: "
+    puts 'Rentals: '
 
-    found_person = @people.select {|person| person.id == id}
+    found_person = @people.select { |person| person.id == id }
 
     if found_person.empty?
-      puts "No one with this ID was found."
+      puts "\nNo one with this ID was found."
+    elsif found_person[0].rentals.empty?
+      puts "\nThis person has no rentals."
     else
       found_person[0].rentals.each do |rental|
         puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
@@ -118,25 +150,25 @@ class App
       puts list_person_rentals
     end
   end
-  
+
   # Run app until the exist option is chosen
-  def run 
-    puts "============= Welcome to the School Library App! =============="
+  def run
+    puts '============= Welcome to the School Library App! =============='
     terminate = false
-    
+
     until terminate
       puts @welcome_message
       number = gets.chomp.to_i
-      
-      if number.between?(1,6)
+
+      if number.between?(1, 6)
         user_choice(number)
       elsif number == 7
         terminate = true
         puts "Thanks for using the app. \nExisting..."
-        
+
       else
-        puts "Wrong input. Try again."
+        puts 'Wrong input. Try again.'
       end
-    end     
+    end
   end
 end
--- modules/student.rb
+++ modules/student.rb
@@ -3,13 +3,14 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = nil
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.includes?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
