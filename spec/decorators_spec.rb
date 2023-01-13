1a20bf575fac87f2afd84cab0902518b6bd8124c
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-01-13 16:26:14 +0200

    fix linters errors

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
-​
+
 describe 'Tests for decorators' do
   context 'When testing decorator classes' do
     before :each do
       @person = Person.new(23, 45, 'john stiffen')
     end
-​
+
     describe BaseDecorator do
       it 'should return the argument unchanged' do
         expect(@person.correct_name).to eq 'john stiffen'
       end
     end
-​
+
     describe CapitalizeDecorator do
       it 'should return the argument Capitalized' do
         capital_decorator = CapitalizeDecorator.new(@person)
         expect(capital_decorator.correct_name).to eq 'John stiffen'
       end
     end
-​
+
     describe TrimmerDecorator do
       it 'should return the argument trimmed' do
         trimmer_decorator = TrimmerDecorator.new(@person)
