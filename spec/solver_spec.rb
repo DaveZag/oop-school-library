d69704f3463e762bdc2f5f6a1a9df1abb814f55b
john <kiokoj81@gmail.com>
2023-01-14 14:30:38 +0300

    Add a test for factorials

---
+++ spec/solver_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'solver'
+
+RSpec.describe Solver do
+  context 'Factorial' do
+    it 'Test : Factorial of a number' do
+      result = Solver.new
+      expect(result.factorial(0)).to eq(1)
+      expect(result.factorial(1)).to eq 1
+      expect(result.factorial(2)).to eq 2
+      expect(result.factorial(3)).to eq(3)
+      expect(result.factorial(-2)).to eq('No negative number')
+    end
+  end 
+end
